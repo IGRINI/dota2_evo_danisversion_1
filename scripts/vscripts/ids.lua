c1b8592fbe54d3a063da6d837c0434b5a5868f7f
IGRINI <grigmat78@gmail.com>
2016-08-10 01:57:18 +0300

    Change
    Change OnEntityKilled

--- scripts/vscripts/ids.lua
+++ scripts/vscripts/ids.lua
@@ -1,11 +1,3 @@
 print("Evolution and Grow System")
 
 require('barebones')
-
-function OnEntityKilled(event)
-	local killedUnit = EntIndexToHScript( event.entindex_killed )
-	local killedTeam = killedUnit:GetTeam()
-
-	local killer = EntIndexToHScript( event.entindex_attacker )
-	local killerTeam = hero:GetTeam()
-end

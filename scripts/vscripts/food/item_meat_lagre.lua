b97912323a0e41d952ece8350cc4f3034604157a
IGRINI <grigmat78@gmail.com>
2016-08-11 12:08:28 +0300

    Аргументы
    Аргументы

--- scripts/vscripts/food/item_meat_lagre.lua
+++ scripts/vscripts/food/item_meat_lagre.lua
@@ -3,36 +3,33 @@ if item_meat_lagre == nil then
 end
 LinkLuaModifier( "modifier_meat_med","modifiers/modifiers", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_omnivore_first","modifiers/modifiers", LUA_MODIFIER_MOTION_NONE )
-function item_meat_lagre:GetChannelTime()
+function item_meat_lagre:GetChannelTime()																--Получаем время каста
 	return 5.2
 end
-function item_meat_lagre:OnAbilityPhaseStart()
-	return true
-end
 function item_meat_lagre:OnSpellStart()
-	local caster = self:GetCaster()
-	if not caster:HasModifier("modifier_fullfilled_med") then
-		caster:AddNewModifier(caster,self,"modifier_meat_med",{})
-		if not caster:HasModifier("modifier_omnivore_first") then
-			caster:AddNewModifier(caster,self,"modifier_omnivore_first",{})
-			caster:SetModifierStackCount("modifier_omnivore_first",caster,1)
-		elseif caster:HasModifier("modifier_omnivore_first") then
-			local stacks = caster:GetModifierStackCount("modifier_omnivore_first",caster)
-			caster:SetModifierStackCount("modifier_omnivore_first",caster,stacks + 1)
+	local caster = self:GetCaster()																		--Получаем кастера
+	if not caster:HasModifier("modifier_fullfilled_med") then 											--Если на кастере не висит модификатора "полного живота, то"
+		caster:AddNewModifier(caster,self,"modifier_meat_med",{})										--Добавляем кастеру модификатор, который восстанавливает % хп
+		if not caster:HasModifier("modifier_omnivore_first") then 										--Если кастер не имеет модификатора Плотоядного, добавляем его и увеличиваем количество стаков на 1, чтобы сразу была единичка
+			caster:AddNewModifier(caster,self,"modifier_omnivore_first",{})								--Вот добавление модификатора
+			caster:SetModifierStackCount("modifier_omnivore_first",caster,1) 							--Вот установка стака на
+		elseif caster:HasModifier("modifier_omnivore_first") then  										--Иначе если кастер уже имеет модификатор плотоядного, то
+			local stacks = caster:GetModifierStackCount("modifier_omnivore_first",caster) 				--Получаем сколько стаков на модификаторе
+			caster:SetModifierStackCount("modifier_omnivore_first",caster,stacks + 1)					--Увеличиваем кол-во стаков на 1
 		end
-	else
-		self:RemoveSelf()
-		caster:AddItemByName("item_meat_lagre")
+	else 																								--Если кастер имеет модификатор полного живота на себе, то
+		self:RemoveSelf()																				--Удаляем предмет из инвентаря, чтобы не пошел каст
+		caster:AddItemByName("item_meat_lagre")															--Добавляем предмет обратно(типо он не заюзался)
 	end
 end
-function item_meat_lagre:OnChannelFinish( bInterrupted )
-	local caster = self:GetCaster()
-	if bInterrupted then
-		caster:RemoveModifierByName("modifier_meat_med")
-		self:RemoveSelf()
-		caster:AddNewModifier(caster,self,"modifier_fullfilled_med",{duration = 16})
-	else
-		caster:RemoveModifierByName("modifier_meat_med")
+function item_meat_lagre:OnChannelFinish( bInterrupted )												--Функция, которая запускается, когда каст окончен или прерван
+	local caster = self:GetCaster()																		--Получаем кастера
+	if bInterrupted then 																				--Если каст прерван(салом, станом, сам игрок прервал, чем угодно)
+		caster:RemoveModifierByName("modifier_meat_med")												--Удаляем модификатор хила
+		self:RemoveSelf()																				--Удаляем предмет
+		caster:AddNewModifier(caster,self,"modifier_fullfilled_med",{duration = 16})					--Вешаем модификатор полного живота на 16 сек
+	else 																								--Если каст дошел до конца
+		caster:RemoveModifierByName("modifier_meat_med")												--То же самое, просто если не определить, что будет делаться при прерыве каста, модификатор хила так и останется висеть на кастере
 		self:RemoveSelf()
 		caster:AddNewModifier(caster,self,"modifier_fullfilled_med",{duration = 16})
 	end

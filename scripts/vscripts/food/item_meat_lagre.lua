31f989dabe4281f9a6889ecb191aa006f71196a0
IGRINI <grigmat78@gmail.com>
2016-08-11 12:48:42 +0300

    Смотри
    Смотри

--- scripts/vscripts/food/item_meat_lagre.lua
+++ scripts/vscripts/food/item_meat_lagre.lua
@@ -6,21 +6,35 @@ LinkLuaModifier( "modifier_carnivore_first","modifiers/modifiers", LUA_MODIFIER_
 function item_meat_lagre:GetChannelTime()																--Получаем время каста
 	return 5.2
 end
+function item_meat_lagre:CastFilterResult()
+	local caster = self:GetCaster()
+	if caster:HasModifier("modifier_fullfilled_med") then
+		return UF_FAIL_CUSTOM
+	end
+	return UF_SUCCESS
+end
+function item_meat_lagre:GetCustomCastError()
+	local caster = self:GetCaster()
+	if caster:HasModifier("modifier_fullfilled_med") then
+		return "#error_you_filled"
+	end
+	return ""
+end
 function item_meat_lagre:OnSpellStart()
 	local caster = self:GetCaster()																		--Получаем кастера
-	if not caster:HasModifier("modifier_fullfilled_med") then 											--Если на кастере не висит модификатора "полного живота, то"
-		caster:AddNewModifier(caster,self,"modifier_meat_med",{})										--Добавляем кастеру модификатор, который восстанавливает % хп
-		if not caster:HasModifier("modifier_carnivore_first") then 										--Если кастер не имеет модификатора Плотоядного, добавляем его и увеличиваем количество стаков на 1, чтобы сразу была единичка
-			caster:AddNewModifier(caster,self,"modifier_carnivore_first",{})								--Вот добавление модификатора
-			caster:SetModifierStackCount("modifier_carnivore_first",caster,1) 							--Вот установка стака на
-		elseif caster:HasModifier("modifier_carnivore_first") then  										--Иначе если кастер уже имеет модификатор плотоядного, то
-			local stacks = caster:GetModifierStackCount("modifier_carnivore_first",caster) 				--Получаем сколько стаков на модификаторе
-			caster:SetModifierStackCount("modifier_carnivore_first",caster,stacks + 1)					--Увеличиваем кол-во стаков на 1
-		end
-	else 																								--Если кастер имеет модификатор полного живота на себе, то
+	--if not caster:HasModifier("modifier_fullfilled_med") then 										--Если на кастере не висит модификатора "полного живота, то"
+	caster:AddNewModifier(caster,self,"modifier_meat_med",{})											--Добавляем кастеру модификатор, который восстанавливает % хп
+	if not caster:HasModifier("modifier_carnivore_first") then 											--Если кастер не имеет модификатора Плотоядного, добавляем его и увеличиваем количество стаков на 1, чтобы сразу была единичка
+		caster:AddNewModifier(caster,self,"modifier_carnivore_first",{})								--Вот добавление модификатора
+		caster:SetModifierStackCount("modifier_carnivore_first",caster,1) 								--Вот установка стака на
+	elseif caster:HasModifier("modifier_carnivore_first") then  										--Иначе если кастер уже имеет модификатор плотоядного, то
+		local stacks = caster:GetModifierStackCount("modifier_carnivore_first",caster) 					--Получаем сколько стаков на модификаторе
+		caster:SetModifierStackCount("modifier_carnivore_first",caster,stacks + 1)						--Увеличиваем кол-во стаков на 1
+	end
+	--[[else 																							--Если кастер имеет модификатор полного живота на себе, то
 		self:RemoveSelf()																				--Удаляем предмет из инвентаря, чтобы не пошел каст
 		caster:AddItemByName("item_meat_lagre")															--Добавляем предмет обратно(типо он не заюзался)
-	end
+	end]]
 end
 function item_meat_lagre:OnChannelFinish( bInterrupted )												--Функция, которая запускается, когда каст окончен или прерван
 	local caster = self:GetCaster()																		--Получаем кастера

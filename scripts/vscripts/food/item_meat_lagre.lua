abbf214bd21c29451d961da560f348ec70680345
IGRINI <grigmat78@gmail.com>
2016-08-11 12:14:55 +0300

    Тогда так
    Тогда так

--- scripts/vscripts/food/item_meat_lagre.lua
+++ scripts/vscripts/food/item_meat_lagre.lua
@@ -2,7 +2,7 @@ if item_meat_lagre == nil then
 	item_meat_lagre = class({})
 end
 LinkLuaModifier( "modifier_meat_med","modifiers/modifiers", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_omnivore_first","modifiers/modifiers", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_carnivore_first","modifiers/modifiers", LUA_MODIFIER_MOTION_NONE )
 function item_meat_lagre:GetChannelTime()																--Получаем время каста
 	return 5.2
 end
@@ -10,12 +10,12 @@ function item_meat_lagre:OnSpellStart()
 	local caster = self:GetCaster()																		--Получаем кастера
 	if not caster:HasModifier("modifier_fullfilled_med") then 											--Если на кастере не висит модификатора "полного живота, то"
 		caster:AddNewModifier(caster,self,"modifier_meat_med",{})										--Добавляем кастеру модификатор, который восстанавливает % хп
-		if not caster:HasModifier("modifier_omnivore_first") then 										--Если кастер не имеет модификатора Плотоядного, добавляем его и увеличиваем количество стаков на 1, чтобы сразу была единичка
-			caster:AddNewModifier(caster,self,"modifier_omnivore_first",{})								--Вот добавление модификатора
-			caster:SetModifierStackCount("modifier_omnivore_first",caster,1) 							--Вот установка стака на
-		elseif caster:HasModifier("modifier_omnivore_first") then  										--Иначе если кастер уже имеет модификатор плотоядного, то
-			local stacks = caster:GetModifierStackCount("modifier_omnivore_first",caster) 				--Получаем сколько стаков на модификаторе
-			caster:SetModifierStackCount("modifier_omnivore_first",caster,stacks + 1)					--Увеличиваем кол-во стаков на 1
+		if not caster:HasModifier("modifier_carnivore_first") then 										--Если кастер не имеет модификатора Плотоядного, добавляем его и увеличиваем количество стаков на 1, чтобы сразу была единичка
+			caster:AddNewModifier(caster,self,"modifier_carnivore_first",{})								--Вот добавление модификатора
+			caster:SetModifierStackCount("modifier_carnivore_first",caster,1) 							--Вот установка стака на
+		elseif caster:HasModifier("modifier_carnivore_first") then  										--Иначе если кастер уже имеет модификатор плотоядного, то
+			local stacks = caster:GetModifierStackCount("modifier_carnivore_first",caster) 				--Получаем сколько стаков на модификаторе
+			caster:SetModifierStackCount("modifier_carnivore_first",caster,stacks + 1)					--Увеличиваем кол-во стаков на 1
 		end
 	else 																								--Если кастер имеет модификатор полного живота на себе, то
 		self:RemoveSelf()																				--Удаляем предмет из инвентаря, чтобы не пошел каст
